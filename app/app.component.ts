eb43e9df576c473fbba0d0b153362f40c3e5e292
Robert Ellis <robertellis.rob@gmail.com>
2016-05-12 14:39:06 -0600

    switched TableAppComponent for ng2-easy-table back to AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,12 @@
 ///<reference path="./../typings/browser/ambient/es6-shim/index.d.ts"/>
 import {Component}     from '@angular/core';
 import {bootstrap}     from '@angular/platform-browser-dynamic';
-import {TableAppComponent}  from 'ng2-easy-table/app/app.component';
+import {AppComponent}  from 'ng2-easy-table/app/app.component';
 import {MyConfigService} from "./config-service";
 
 @Component({
   selector: 'my-app',
-  directives: [TableAppComponent],
+  directives: [AppComponent],
   providers: [MyConfigService],
   template: ` <h3> My use of ng2-easy-table </h3>
     <ng2-table [configuration]="configuration"></ng2-table>
